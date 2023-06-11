48ed7959fec5148ed2aafc7c0bb7d231415f2dd7
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-06-11 13:11:10 -0500

    Ejemplo 3 - Versión sin contar tamaño de arreglo = 1

--- Lab4ej3.java
+++ Lab4ej3.java
@@ -22,20 +22,16 @@ public class Lab4ej3 {
         int key;
         int i;
         long nano_StartTime = System.nanoTime();
-        if (A.length == 1) {
+        for (int j = 1; j < A.length; j++) {
+            key = A[j];
+            i = j - 1;
             imprimirArreglo(A);
-        } else {
-            for (int j = 1; j < A.length; j++) {
-                key = A[j];
-                i = j - 1;
-                imprimirArreglo(A);
-                while (i >= 0 && A[i] > key) {
-                    A[i + 1] = A[i];
-                    i--;
-                }
-                A[i + 1] = key;
-                imprimirArreglo(A);
+            while (i >= 0 && A[i] > key) {
+                A[i + 1] = A[i];
+                i--;
             }
+            A[i + 1] = key;
+            imprimirArreglo(A);
         }
         long nano_EndTime = System.nanoTime();
         return nano_EndTime - nano_StartTime;
